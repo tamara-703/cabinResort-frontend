ef3dedac6ca7f119555d8f3f8cb845e59d56f601
TMastSkillStorm <tmast@skillstorm.com>
2023-09-19 16:55:46 -0400

    added service classes for log-in and user. added some html elements. TODO fix log in 401

---
+++ src/app/environment.ts
@@ -0,0 +1,8 @@
+export const Environment = {
+    production: false,
+    //QA URL
+    //EnvironmentURL: "http://project2-cabinfever-env.eba-iangfsb2.us-east-1.elasticbeanstalk.com:8080"
+
+    //Dev URL
+    EnvironmentURL: "http://localhost:8080"
+}
---
+++ src/app/log-in/services/log-in.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { GuestId } from 'src/app/users';
+import { MessageService } from 'src/app/message.service';
+import { Environment } from 'src/app/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogInService { private userUrl = Environment.EnvironmentURL; //Gets URL based on environment 
+
+httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json', 'allow': 'PUT' })
+};
+
+constructor(
+  private http: HttpClient,
+  private messageService: MessageService) {}
+
+
+  
+    /** GET User by id. Will 404 if id not found */
+    getUser(): Observable<GuestId> {
+      const url = `${this.userUrl}/user/profile`;
+      return this.http.get<GuestId>(url).pipe(
+        tap(_ => this.log(`fetched User`)),
+        catchError(this.handleError<GuestId>(`unable to get users`))
+      );
+    }
+     /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a UserService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`UserService: ${message}`);
+  }
+
+}
