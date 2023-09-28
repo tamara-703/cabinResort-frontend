f3979e975bab706b91289d9995d69f4e8a9b9f04
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-09-28 18:06:11 -0400

    Added amenities icons in the cabins component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { CalendarModule } from 'primeng/calendar';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { ToastModule } from 'primeng/toast';
 import { InputTextModule } from 'primeng/inputtext';
+import { MatIconModule } from '@angular/material/icon'
 
 const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
       // ......
@@ -60,7 +61,8 @@ const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
     ReactiveFormsModule,
     ToastModule,
     InputTextModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatIconModule
   ],
   providers: [{
     provide: {
